acaa0c0a2709f52e5507e5cde45ce7a64b2c5ebe
lucasvlviana <70477354+lucasvlviana@users.noreply.github.com>
2022-06-24 22:16:10 -0300

    90% do curso avançado

--- PHP/Avançado/listas.php
+++ PHP/Avançado/listas.php
@@ -1,6 +1,11 @@
 <?php
 
-$idadeList = [21, 23, 19,25, 30, 41,18];
-$umaIdade = $idadeList[2];
+$idadeList = [21, 23, 19, 25, 30, 41, 18];
 
-echo $umaIdade;
+list($idadeVinicius, $idadeJoao, $idadeMaria) = $idadeList;
+
+$idadeList[] = 20;
+
+foreach ($idadeList as $idade) {
+    echo $idade . PHP_EOL;
+}
--- PHP/Avançado/loop-lista.php
+++ PHP/Avançado/loop-lista.php
@@ -1,7 +1,6 @@
 <?php
 
-$idadeList = [21, 23, 19,25, 30, 41,18];
-
+$idadeList = [23, 19, 25, 30, 41, 18, 21, 35];
 
 for ($i = 0; $i < count($idadeList); $i++) {
     echo $idadeList[$i] . PHP_EOL;
