00ca9db65cc29a9820bbeb865388bbc82c08680e
Lucas Viana <lucasvitorlv@gmail.com>
2022-06-28 11:45:20 -0300

    subindo arquivo do curso de POO

--- PHP/POO/conta.php
+++ PHP/POO/conta.php
@@ -1,10 +1,8 @@
 <?php
 
-function criarConta(string $cpf, string $nomeTitualar, float $saldo): array {
-    return [
-        $cpf => [
-            'titular' => $nomeTitualar,
-            'saldo' => $saldo,
-        ]
-        ];
+class Conta
+{
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo;
 }
