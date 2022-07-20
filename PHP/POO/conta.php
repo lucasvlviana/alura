611dbc571fa919bdcc55f67b960dbe1082a45d4f
Lucas Viana <lucasvitorlv@gmail.com>
2022-07-20 17:36:05 -0300

    material de POO

--- PHP/POO/conta.php
+++ PHP/POO/conta.php
@@ -4,14 +4,35 @@ class Conta
 {
     public $cpfTitular;
     public $nomeTitular;
-    public $saldo;
+    public $saldo = 0;
 }
 
-public function sacar( float $valorASacar)
+public function sacar( float $valorASacar): void
 {
     if ($valorASacar > $this->$saldo) {
         echo "saldo inisponível";
     } else {
         $this->$saldo =- $valorASacar;
     }
+}
+
+public function depositar(float $valorADepositar): void
+{
+    if ($valorADepositar < 0) {
+        echo "Valor precisa ser positivo";
+        return;
+    }
+
+    $this->saldo += $valorADepositar;
+}
+
+public function transferir(float $valorATransferir, Conta $contaDestino): void
+{
+    if ($valorATransferir > $this->saldo) {
+        echo "Saldo indisponível";
+        return;
+    } 
+
+    $this->sacar($valorATransferir);
+    $contaDestino->depositar($valorATransferir);
 }
